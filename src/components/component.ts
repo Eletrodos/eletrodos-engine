678e73ed2aed9363c97afcb4d37bc7e83d18ef2c
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2019-08-27 14:10:05 -0300

    Implementa função de atualização para engine

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,6 +1,6 @@
 export interface IComponent {
-    /** Chamado quando o objeto é construido */
-    start: () => void,
-    /** Chamado dentro do loop de renderização da engine */
-    update: () => void
+	/** Chamado quando o objeto é construido */
+	start: () => void,
+	/** Chamado dentro do loop de renderização da engine */
+	update: () => void
 }
